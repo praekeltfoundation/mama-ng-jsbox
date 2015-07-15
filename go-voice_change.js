f347d68976dd4d04e6b929091c5057886e6bdf5b
gsvr <george@westerncapelabs.com>
2015-07-15 17:26:10 +0200

    note temporary hack

--- go-voice_change.js
+++ go-voice_change.js
@@ -55,6 +55,7 @@ go.utils = {
     },
 
     // Determine whether contact is registered
+    // TODO #12 - this is currently just a temporary workaround
     is_registered: function(im) {
         return Q()
             .then(function() {
