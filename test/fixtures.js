396f46d91736278ceb991bf33e86bbb64b4b0592
gsvr <george@westerncapelabs.com>
2015-09-03 17:11:45 +0200

    remove unused fixture

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,39 +1,5 @@
 module.exports = function() {
     return [
-        // get contact 07030010001 by msisdn
-        {
-            'request': {
-                'method': 'GET',
-                'params': {
-                    'msisdn': '+2347030010001'
-                },
-                'headers': {
-                    'Authorization': ['Token test_key'],
-                    'Content-Type': ['application/json']
-                },
-                'url': 'http://localhost:8000/api/v1/contacts/search/',
-            },
-            'response': {
-                "code": 200,
-                "data": {
-                    "count": 1,
-                    "next": null,
-                    "previous": null,
-                    "results": [{
-                        "url": "http://localhost:8000/api/v1/contacts/cb245673-aa41-4302-ac47-00000000001/",
-                        "id": "cb245673-aa41-4302-ac47-00000000001",
-                        "version": 1,
-                        "details": {
-                            "default_addr_type": "msisdn",
-                            "addresses": "msisdn:+07030010001"
-                        },
-                        "created_at": "2015-07-10T06:13:29.693272Z",
-                        "updated_at": "2015-07-10T06:13:29.693298Z"
-                    }]
-                }
-            }
-        },
-
         // get contact 08080020002 by msisdn
         {
             'request': {
