954c86d8ff720e35531e0d1c89036783eab4f7ce
gsvr <george@westerncapelabs.com>
2016-03-15 19:21:38 +0200

    reinstate grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,9 +155,9 @@ module.exports = function (grunt) {
             options: {
                 reporter: 'spec'
             },
-            // test_ussd_registration: {
-            //     src: ['<%= paths.test.ussd_registration %>']
-            // },
+            test_ussd_registration: {
+                src: ['<%= paths.test.ussd_registration %>']
+            },
             test_voice_registration: {
                 src: ['<%= paths.test.voice_registration %>']
             },
