3531a00899c812f3b4393088057fb92318c0e383
gsvr <george@westerncapelabs.com>
2016-03-17 12:08:49 +0200

    reinstate skipped tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,20 +155,20 @@ module.exports = function (grunt) {
             options: {
                 reporter: 'spec'
             },
-            // test_ussd_registration: {
-            //     src: ['<%= paths.test.ussd_registration %>']
-            // },
-            // test_voice_registration: {
-            //     src: ['<%= paths.test.voice_registration %>']
-            // },
+            test_ussd_registration: {
+                src: ['<%= paths.test.ussd_registration %>']
+            },
+            test_voice_registration: {
+                src: ['<%= paths.test.voice_registration %>']
+            },
             test_ussd_public: {
                 src: ['<%= paths.test.ussd_public %>']
-            // },
-            // test_voice_public: {
-            //     src: ['<%= paths.test.voice_public %>']
-            // },
-            // test_sms_inbound: {
-            //      src: ['<%= paths.test.sms_inbound %>']
+            },
+            test_voice_public: {
+                src: ['<%= paths.test.voice_public %>']
+            },
+            test_sms_inbound: {
+                 src: ['<%= paths.test.sms_inbound %>']
             }
         }
     });
